98f736cce7709c953ca2802084d1ab19ed45d3cc
Samarth Shah <shahsamarth1991@gmail.com>
2016-11-27 02:12:20 +0530

    delete drolet flow

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-var accessToken = 'd35d0c0bd150fbbad761b572432aef4ed2b642ef001f04d9d78b63d19555995b';
+var accessToken = '1d54dec1716d7c6a96a16ee33507b80b0a27b95db57670efcabaa4cb9e7fece4';
 var DIGITALOCEAN = require('dropletapi').Droplets;
 var model = 'https://api.projectoxford.ai/luis/v2.0/apps/ef759c58-7bca-4c77-ba28-e40146764b0b?subscription-key=dfa71d213f1b4645b249e95ece228af6'
 var CODE = '963c3bc73ac6fe55c4278c38aeb3c1fa97937fe6d721fd74bb5304dcfec8eca0';
 var CLIENT_ID = 'bc9ddc5faa820489de13ac23f998fe79d55a9e880b5faaa0353321630539822d';
 var CLIENT_SECRET = '257385f6b195fc8854255219540e0ff0f22d42ad2a09b5e8a2400440b0322e84';
 var CREATE_DROPLET_DATA = {
-    "name":'hostname',
-    "region": ['nyc1','nyc2','nyc3','sfo1','ams2','sgp1','lon1','nyc3','ams3','fra1','tor1','sfo2','blr1'],
-    "size": ['512mb','1gb'],
-    "images": ['ubuntu-14-04-x64','ubuntu-14-04-x32']
+    "name": 'hostname',
+    "region": ['nyc1', 'nyc2', 'nyc3', 'sfo1', 'ams2', 'sgp1', 'lon1', 'nyc3', 'ams3', 'fra1', 'tor1', 'sfo2', 'blr1'],
+    "size": ['512mb', '1gb'],
+    "images": ['ubuntu-14-04-x64', 'ubuntu-14-04-x32']
 }
 
 app.get('/callback', function (req, res) {
@@ -41,9 +41,12 @@ app.get('/home', function (req, res) {
             if (!error && response.statusCode == 200) {
                 console.log(JSON.parse(body).topScoringIntent);
                 var detectedIntent = JSON.parse(body).topScoringIntent.intent;
-                if (detectedIntent = 'createDroplet') {
-                    res.setHeader('content-type', 'application/JSON')
+                if (detectedIntent == 'createDroplet') {
+                    res.setHeader('content-type', 'application/JSON');
                     res.send(CREATE_DROPLET_DATA);
+                } else if (detectedIntent == 'deleteDroplet') {
+                    getAllDropletDetails(res);
+                    //deleteDroplet(dropletID);
                 } else {
                     res.send(detectedIntent);
                 }
@@ -55,14 +58,16 @@ app.get('/home', function (req, res) {
         var region = JSON.parse(req.query.createDropletData).region;
         var size = JSON.parse(req.query.createDropletData).size;
         var image = JSON.parse(req.query.createDropletData).images;
-        createDroplet(name, region, size, image);
+        createDroplet(name, region, size, image, res);
     }
-    else {
+    else if (req.query.deleteDropletId != undefined) {
+        deleteDroplet(req.query.deleteDropletId,res);
+    } else {
         res.send("please send search query.")
     }
 })
 
-function createDroplet(name,region,size,image) {
+function createDroplet(name, region, size, image, res) {
     var digitalocean = new DIGITALOCEAN(accessToken);
     var myNewDropletData = {
         "name": name,
@@ -82,11 +87,45 @@ function createDroplet(name,region,size,image) {
             res.send(error);
         }
         else {
-            res.send(result);
             console.log(result);
+            res.send(result);
         }
     });
 };
+
+function deleteDroplet(dropletID, res) {
+    var digitalocean = new DIGITALOCEAN(accessToken);
+    digitalocean.deleteDroplet(dropletID, function (error, result) {
+        if (error) {
+            console.log(error);
+            res.send(error);
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+function getAllDropletDetails(res) {
+    var digitalocean = new DIGITALOCEAN(accessToken);
+
+    digitalocean.listDroplets(function (error, result) {
+        if (error) {
+            console.log(error);
+            return error;
+        }
+        else {
+            console.log(result);
+            res.setHeader('content-type', 'application/JSON');
+            var releventData = [];
+            for (var i = 0; i < result.droplets.length; i++) {
+                releventData.push({ name: result.droplets[i].name, id: result.droplets[i].id });
+            }
+            res.send(JSON.stringify(releventData));
+        }
+    });
+}
 
 app.post('/home', function (req, res) {
     res.send("At this moment, it is not defined.");
