53b47a5b6476dccef4079c643abfed77c9364546
SAPIENT\ssh204 <sshah12@sapient.com>
2016-11-26 14:40:28 +0530

    included accessToken , updated home route

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var accessToken="";
+
+var model ='https://api.projectoxford.ai/luis/v2.0/apps/ef759c58-7bca-4c77-ba28-e40146764b0b?subscription-key=dfa71d213f1b4645b249e95ece228af6'
 
 app.get('/callback', function (req, res) {
-    res.send("callback")
+    accessToken=req.query.code;
+    console.log(accessToken);
+    res.send("Access Token");
 })
 
-app.get('/callback', function (req, res) {
-    request('https://api.projectoxford.ai/luis/v2.0/apps/d4d18f70-d9f8-4f71-9b0a-d5304750a7ef?subscription-key=dfa71d213f1b4645b249e95ece228af6&q=%27sam%27', function (error, response, body) {
+app.post('/accessToken',function(req,res){
+    res.send(accessToken);
+})
+
+app.get('/home', function (req, res) {
+    request(model+'&q='+req.query.searchQuery, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(JSON.parse(body).topScoringIntent)// Show the HTML for the Google homepage. 
         }
-        res.send('Hi')
+        res.send('Thanks. We are processing your query.');
     })
+})
+
+app.post('/home',function(req,res){
+    res.send("At this moment, it is not defined.");
 })
 
 var server = app.listen(8083, function () {
