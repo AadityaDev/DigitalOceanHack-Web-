17772e0dac33027c2de37bf570edb938d75a5722
SAPIENT\ssh204 <sshah12@sapient.com>
2016-11-26 18:13:57 +0530

    updated res type for callback

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-var accessToken = "";
+var accessToken = 'd35d0c0bd150fbbad761b572432aef4ed2b642ef001f04d9d78b63d19555995b';
 var DIGITALOCEAN = require('dropletapi').Droplets;
 var model = 'https://api.projectoxford.ai/luis/v2.0/apps/ef759c58-7bca-4c77-ba28-e40146764b0b?subscription-key=dfa71d213f1b4645b249e95ece228af6'
-var CODE = 'd9ab741b2548b181ef4b5e31bd57e2549f6200b838c42fbb03196fe07e4805c5';
+var CODE = '963c3bc73ac6fe55c4278c38aeb3c1fa97937fe6d721fd74bb5304dcfec8eca0';
 var CLIENT_ID = 'bc9ddc5faa820489de13ac23f998fe79d55a9e880b5faaa0353321630539822d';
 var CLIENT_SECRET = '257385f6b195fc8854255219540e0ff0f22d42ad2a09b5e8a2400440b0322e84';
 
@@ -15,11 +15,12 @@ app.get('/callback', function (req, res) {
         request.post(URL, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 accessToken = JSON.parse(body).accessToken;
-                res.send(JSON.parse(body));
+                res.setHeader('content-type','application/JSON')
+                res.send(body);
             }
         })
     }
-    else{
+    else {
         res.send("please send code");
     }
 })
@@ -29,12 +30,39 @@ app.post('/accessToken', function (req, res) {
 })
 
 app.get('/home', function (req, res) {
-    request(model + '&q=' + req.query.searchQuery, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(JSON.parse(body).topScoringIntent)// Show the HTML for the Google homepage. 
-        }
-        res.send('Thanks. We are processing your query.');
-    })
+    if (req.query.searchQuery != undefined) {
+        request(model + '&q=' + req.query.searchQuery, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(JSON.parse(body).topScoringIntent)
+                var digitalocean = new DIGITALOCEAN(accessToken);
+                var myNewDropletData = {
+                    "name": "example.com",
+                    "region": "nyc3",
+                    "size": "512mb",
+                    "image": "ubuntu-14-04-x64",
+                    "ssh_keys": null,
+                    "backups": false,
+                    "ipv6": true,
+                    "user_data": null,
+                    "private_networking": null
+                }
+
+                digitalocean.createDroplet(myNewDropletData, function (error, result) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        console.log(result);
+                    }
+                });
+                res.send(JSON.parse(body).topScoringIntent.intent);
+            }
+
+        })
+    }
+    else {
+        res.send("please send search query.")
+    }
 })
 
 app.post('/home', function (req, res) {
